5ba40d55e29267b7b6d6afe6c33bdf5bc90d44ed
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-11-07 11:36:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ enterBtn.addEventListener("keyup", function(event)
   {
     event.preventDefault;
     if(event.keyCode === 13){
-      console.log("hello")
+      promptTheUser();
     }
   });
+
+function promptTheUser(){
+  var input = document.getElementById("myText").value;
+  document.getElementById("output").innerHTML += ">>" + input + "</br>"
+}
