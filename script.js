325f1060a30479dcd3d08efebd14c0d68d021e93
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-11-08 11:17:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,5 +12,10 @@ enterBtn.addEventListener("keyup", function(event)
 function promptTheUser(){
   var input = document.getElementById("myText").value;
  output.innerHTML += ">>" + input + "</br>"
+  switch(input){
+      case"g":
+        output.innerHTML += "What direction would you like to go?</br><small>(e-East s-South w-West n-North)"
+      break
+  }//end switch
   output.scrollTop = output.scrollHeight
-}
+}//end promptTheUser
