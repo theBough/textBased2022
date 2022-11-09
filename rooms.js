18dfeeee1e761dfded5a24057273d370700cb78e
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-11-09 11:37:53 -0600

    Create rooms.js

---
+++ rooms.js
@@ -0,0 +1,12 @@
+//Please customize the room names and description to 
+//suit your game.
+
+var rooms = [];
+function loadRooms(){
+  rooms = [
+    [eastTower,foyer,westTower]
+  ]
+}
+var eastTower = "You are in the East Tower </br>"
+var westTower = "You are in the West Tower </br>"
+var foyer = "You are in the foyer </br>"
