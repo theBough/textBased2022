192582ef370e81c0db2a87858fd48a6ad7d7ccda
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-11-22 11:04:44 -0600

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1,13 +1,32 @@
 var rooms = [];
 function loadRooms(){
   rooms = [
-    [eastTower,foyer,westTower],
+    [westTower,foyer,eastTower],
     [dungeon,library,throne]
   ]
+ 
 }
-var eastTower = "You are in the East Tower </br>"
-var westTower = "You are in the West Tower </br>"
-var foyer = "You are in the foyer </br>"
-var dungeon = "You are in the dungeon, it stinks </br>"
-var library = "You are in the library, wanna read something </br>"
-var throne = "You are sitting in a throne. Congrats </br>"
+var eastTower = {
+  name:"East Tower",
+  description:  "You are in the East Tower </br>"
+}
+var westTower ={
+  name:"West Tower",
+  description:"You are in the West Tower </br>"
+} 
+var foyer ={
+  name:"Foyer",
+  description:"You are in the foyer </br>"
+} 
+var dungeon ={
+  name:"Dungeon",
+  description:"You are in the dungeon, it stinks </br>"
+} 
+var library ={
+  name: "Library",
+  description:"You are in the library, wanna read something </br>"
+} 
+var throne ={
+  name:"Throne",
+  description:"You are sitting in a throne. Congrats </br>"
+} 
